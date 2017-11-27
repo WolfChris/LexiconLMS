1194a9c5043eab3c3d116d18e75095e72bdf11ae
Tensae <selamten@gmail.com>
2017-11-27 12:38:51 +0100

    Redirect by roles

--- LexiconLMS/Migrations/Configuration.cs
+++ LexiconLMS/Migrations/Configuration.cs
@@ -1,5 +1,8 @@
 namespace LexiconLMS.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -26,6 +29,30 @@ namespace LexiconLMS.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            var RoleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            string role = "Teacher";
+            string password = "Password@1234";
+
+            //Create Role Admin if it does not exist
+            if (!RoleManager.RoleExists(role))
+            {
+                var roleresult = RoleManager.Create(new IdentityRole(role));
+            }
+
+            //Create Admin users with password=123456
+            var admin1 = new ApplicationUser();
+            admin1.UserName = "admin1@admin1.com";
+            admin1.Email = "admin1@admin1.com";
+            admin1.EmailConfirmed = true;
+            UserManager.Create(admin1, password);
+            admin1 = context.Users.FirstOrDefault(x => x.UserName == admin1.UserName);
+
+
+            UserManager.AddToRole(admin1.Id, role);
+
+
+            context.SaveChanges();
         }
     }
 }
---
+++ LexiconLMS/Models/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LexiconLMS.Models
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+        public DateTime CoStartDate { get; set; }
+        public DateTime CoEndDate { get; set; }
+
+        public string Description { get; set; }
+    }
+}
