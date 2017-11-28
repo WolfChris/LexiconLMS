15803a6767447e5b634835fdde836f05051d0c92
User <User@DESK-CLZ7J32>
2017-11-28 09:13:18 +0100

    merge fm git

---
+++ LexiconLMS/Models/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LexiconLMS.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string CourseName { get; set; }
+        public string Description { get; set; }
+        public DateTime CStartDate { get; set; }
+        public DateTime CEndDate { get; set; }
+
+    }
+}
