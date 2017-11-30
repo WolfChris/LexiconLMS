3fe2aaaebbb30202bd5c26e34c3d6dcb559888fb
User <User@DESK-CLZ7J32>
2017-11-30 14:34:48 +0100

    merge t dev

--- LexiconLMS/Migrations/Configuration.cs
+++ LexiconLMS/Migrations/Configuration.cs
@@ -29,35 +29,12 @@ namespace LexiconLMS.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
-            //var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
-            //var RoleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
-            //string role = "Teacher";
-            //string password = "Password@1234";
-
-            ////Create Role Admin if it does not exist
-            //if (!RoleManager.RoleExists(role))
-            //{
-            //    var roleresult = RoleManager.Create(new IdentityRole(role));
-            //}
-
-            ////Create Admin users with password=123456
-            //var admin1 = new ApplicationUser();
-            //admin1.UserName = "admin1@admin1.com";
-            //admin1.Email = "admin1@admin1.com";
-            //admin1.EmailConfirmed = true;
-            //UserManager.Create(admin1, password);
-            //admin1 = context.Users.FirstOrDefault(x => x.UserName == admin1.UserName);
-
-
-            //UserManager.AddToRole(admin1.Id, role);
-
-
-            //context.SaveChanges();
-
             var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
             var RoleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
-            string role = "Student";
-            string password = "Student@123";
+
+
+            string role = "Teacher";
+            string password = "Password@1234";
 
             //Create Role Admin if it does not exist
             if (!RoleManager.RoleExists(role))
@@ -66,18 +43,44 @@ namespace LexiconLMS.Migrations
             }
 
             //Create Admin users with password=123456
-            var student1 = new ApplicationUser();
-            student1.UserName = "student@admin.com";
-            student1.Email = "student@admin.com";
-            student1.EmailConfirmed = true;
-            UserManager.Create(student1, password);
-            student1 = context.Users.FirstOrDefault(x => x.UserName == student1.UserName);
+            var admin1 = new ApplicationUser();
+            admin1.UserName = "admin1@admin1.com";
+            admin1.Email = "admin1@admin1.com";
+            admin1.EmailConfirmed = true;
+            UserManager.Create(admin1, password);
+            admin1 = context.Users.FirstOrDefault(x => x.UserName == admin1.UserName);
 
 
-            UserManager.AddToRole(student1.Id, role);
-
-
+            UserManager.AddToRole(admin1.Id, role);
+           
             context.SaveChanges();
+
+
+
+            //var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            //var RoleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            //string role = "Student";
+            //string password = "Student@123";
+
+            ////Create Role Admin if it does not exist
+            //if (!RoleManager.RoleExists(role))
+            //{
+            //    var roleresult = RoleManager.Create(new IdentityRole(role));
+            //}
+
+            ////Create Admin users with password=123456
+            //var student1 = new ApplicationUser();
+            //student1.UserName = "student@admin.com";
+            //student1.Email = "student@admin.com";
+            //student1.EmailConfirmed = true;
+            //UserManager.Create(student1, password);
+            //student1 = context.Users.FirstOrDefault(x => x.UserName == student1.UserName);
+
+
+            //UserManager.AddToRole(student1.Id, role);
+
+
+            //context.SaveChanges();
 
 
             
