6a99b5805b5d17df2b70e1ce1a9c1d6d13d10556
koljaKapten <ola.bjelving@brandreality.se>
2017-12-08 07:53:20 +0100

    Changed varningmessage new modul and displayformat for datetime

--- LexiconLMS/Models/Activity.cs
+++ LexiconLMS/Models/Activity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
@@ -10,7 +11,9 @@ namespace LexiconLMS.Models
     {
         public int Id { get; set; }
         public string ActivityName { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ActivityStart { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ActivityEnd { get; set; }
         public string ActivityDescription { get; set; }
 
--- LexiconLMS/Models/Modul.cs
+++ LexiconLMS/Models/Modul.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,9 @@ namespace LexiconLMS.Models
         public int Id { get; set; }
         public string ModulName { get; set; }
         public string ModulDescription { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ModulStart { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ModulEnd { get; set; }
        
         public int Courseid { get; set; }
