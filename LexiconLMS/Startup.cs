e17d7f780a274f1e44e5a75187efb64654cb4d7e
User <User@DESK-CLZ7J32>
2017-12-08 13:28:16 +0100

    Student Show  Moduls

--- LexiconLMS/Startup.cs
+++ LexiconLMS/Startup.cs
@@ -27,71 +27,39 @@ namespace LexiconLMS
             //if (!roleManager.RoleExists("Teacher"))
             //{
 
-            //    //first we create Admin rool
-            //    var role = new IdentityRole();
-            //    role.Name = "Teacher";
-            //    roleManager.Create(role);
-
-            //Here we create a Admin super user who will maintain the website
-
-            //    var user = new ApplicationUser();
-            //    user.UserName = "admin@gmail.com";
-            //    user.Email = "admin@gmail.com";
-
-            //    string userPWD = "Admin@gmail.com";
-
-            //    var chkUser = UserManager.Create(user, userPWD);
-
-            //    //Add default User to Role Admin   
-            //    if (chkUser.Succeeded)
-            //    {
-            //        var result1 = UserManager.AddToRole(user.Id, "Teacher");
-
-            //    }
-            //}
-
-            //        var user = new ApplicationUser();
-            //        user.UserName = "student@gmail.com";
-            //        user.Email = "student@gmail.com";
-
-            //        string userPWD = "Student@gmail.com";
-
-            //        var chkUser = UserManager.Create(user, userPWD);
-
-            //        if (chkUser.Succeeded)
-            //        {
-            //            var result1 = UserManager.AddToRole(user.Id, "Student");
-
-            //        }
-        }
+                //first we create Admin rool
+                var role = new IdentityRole();
+                role.Name = "Teacher";
+                roleManager.Create(role);
+            }
 
     }
 }
 
-// creating Creating Student role    
-//if (!roleManager.RoleExists("Student"))
-//{
-//    var role = new IdentityRole();
-//    role.Name = "Student";
-//    roleManager.Create(role);
+        // creating Creating Student role    
+        //if (!roleManager.RoleExists("Student"))
+        //{
+        //    var role = new IdentityRole();
+        //    role.Name = "Student";
+        //    roleManager.Create(role);
 
-//}
-//**admin1 = Password@1234
+        //}
+        //**admin1 = Password@1234
 
-//    if (!roleManager.RoleExists("Teacher"))
-//    {
-//        var role = new IdentityRole();
-//        role.Name = "Teacher";
-//        roleManager.Create(role);
+        //    if (!roleManager.RoleExists("Teacher"))
+        //    {
+        //        var role = new IdentityRole();
+        //        role.Name = "Teacher";
+        //        roleManager.Create(role);
 
-//        var user = new ApplicationUser();
-//        user.UserName = "Teacher1";
-//        user.Email = "teacher@teaches.com";
+        //        var user = new ApplicationUser();
+        //        user.UserName = "Teacher1";
+        //        user.Email = "teacher@teaches.com";
 
-//        string userPWD = "teacher";               
+        //        string userPWD = "teacher";               
 
-//    }
+        //    }
 
-//}
-
-
+        //}
+    
+    
