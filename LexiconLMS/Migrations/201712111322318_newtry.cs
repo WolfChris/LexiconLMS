d97c5dea16fb285c79d03af2e530416839cd9fb7
Chris Wolf <chriswolf70@hotmail.com>
2017-12-11 14:24:31 +0100

    newtry again

---
+++ LexiconLMS/Migrations/201712111322318_newtry.cs
@@ -0,0 +1,18 @@
+namespace LexiconLMS.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class newtry : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.AspNetUsers", "Image");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.AspNetUsers", "Image", c => c.Binary());
+        }
+    }
+}
